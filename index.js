427dac5e5f3c81964b7d0f86ca64ee5f6b88c6bb
github username <github email address>
2018-09-15 19:40:40 -0400

    Done.

--- index.js
+++ index.js
@@ -7,3 +7,8 @@ You should NOT try to redefine any of the above variables.
 */
 
 // Write your code below this line!
+const newID = 1000000000 + oldID;
+const ageIsValid = Number.isInteger(currentAge);
+const randomNumber = Math.random() * 20; //contain a randomly-generated number between 0 (inclusive) and 20 (exclusive).
+const randomInteger = Math.floor(randomNumber); //should take the value in randomNumber and round it down to the nearest integer.
+const randomUserID = randomInteger + 1000000001; //which should convert randomInteger into a valid ID number — an integer between 1000000001 and 1000000020.
